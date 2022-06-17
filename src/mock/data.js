9982e05d7bed676604aa9b18ab32849ed0a6a40b
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-17 01:42:44 -0400

    Add personal data to template
    - About description updated to intro msg
    - Profile image updated to self
    - Starting text updated to self
    - Tab favicon and title updated to self
    - Added link to own resume
    - Set up links to github, linkedin and email
    - Cleaned up footer
    - Removed GithubButtons
    - Changed color scheme to match self

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,29 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Geoffrey Nguyen | Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello, my name is',
+  name: 'Geoff',
+  subtitle: "I'm a Software Developer.",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    "I'm Geoff and I'm a software developer. I love tinkering and devising solutions to challenging tasks, and love to learn new technologies and tools to better equip myself in these pursuits. I also have a passion for helping people and I enjoy seeing the impact of my work in this endeavour.",
+  paragraphTwo:
+    "I'm most comfortable with python, although I have some experience in Java, and I'm looking to implement projects in C++. I'm always looking to learn and improve.",
+  paragraphThree:
+    "Below you can find a collection of projects I'm most proud of. I hope we can connect and collaborate!",
+  resume: 'GeoffreyNguyen_Resume.pdf',
 };
 
 // PROJECTS DATA
@@ -59,7 +62,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'geoffrey.nguyen9@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +70,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://ca.linkedin.com/in/geoffreynguyen77',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/nougat-rey',
     },
   ],
-};
-
-// Github start/fork buttons
-export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
 };
